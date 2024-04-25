ac4a6548476a7397f4d669e2c0c6b0eab738b025
Tudor Comparan <tudor.comparan@gmail.com>
2024-04-25 15:21:56 -0600

    book router

---
+++ books.py
@@ -0,0 +1,80 @@
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+BOOKS = [
+    {"id": 1, "title": "Title 1", "author": "Author 1", "category": "science"},
+    {"id": 2, "title": "Title 2", "author": "Author 2", "category": "science"},
+    {"id": 3, "title": "Title 3", "author": "Author 3", "category": "history"},
+    {"id": 4, "title": "Title 4", "author": "Author 4", "category": "math"},
+    {"id": 5, "title": "Title 5", "author": "Author 5", "category": "math"},
+    {"id": 6, "title": "Title 6", "author": "Author 6", "category": "history"},
+]
+
+
+class CreateBook(BaseModel):
+    title: str
+    author: str
+    category: str
+
+
+class UpdateBook(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    category: Optional[str] = None
+
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    category: str
+
+
+@app.post("/books")
+async def create_book(book: CreateBook) -> Book:
+    book["id"] = len(BOOKS) + 1
+    BOOKS.append(book)
+    return book
+
+
+@app.get("/books")
+async def read_all_books(category: str | None = None) -> list[Book]:
+    if not category:
+        return BOOKS
+
+    filtered_books: list[Book] = []
+
+    for b in BOOKS:
+        if b["category"].casefold() == category.casefold():
+            filtered_books.append(b)
+
+    return filtered_books
+
+
+@app.get("/books/{id}")
+async def read_book_by_id(id: int) -> Book:
+    for b in BOOKS:
+        if b.get("id") == id:
+            return b
+    else:
+        raise HTTPException(status_code=404, detail=f"{id} Not Found")
+
+
+@app.put("/books/{id}")
+async def update_book_by_id(id: int, book: UpdateBook) -> Book:
+
+    for b in BOOKS:
+        if id == b.get("id"):
+            if book.title is not None:
+                b["title"] = book.title
+            if book.author is not None:
+                b["author"] = book.author
+            if book.category is not None:
+                b["category"] = book.category
+
+            return Book(**b)
+
+    raise HTTPException(status_code=404, detail="Book not found")
