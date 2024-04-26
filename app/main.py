b44fce290d0cb33cadb2e7829110e4a7cf4f60d1
Tudor Comparan <tudor.comparan@gmail.com>
2024-04-26 11:55:15 -0600

    prepare code for lection 2, books as a list of Book class

--- app/main.py
+++ app/main.py
@@ -1,34 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
-
-BOOKS = [
-    {
-        "id": 1,
-        "title": "El señor de los anillos",
-        "author": "JRR Tolkien",
-        "category": "novel",
-    },
-    {"id": 2, "title": "El hobbit", "author": "JRR Tolkien", "category": "novel"},
-    {"id": 3, "title": "Title 3", "author": "Author 3", "category": "novel"},
-    {"id": 4, "title": "Title 4", "author": "Author 4", "category": "math"},
-    {"id": 5, "title": "Title 5", "author": "Author 5", "category": "math"},
-    {"id": 6, "title": "Title 6", "author": "Author 6", "category": "history"},
-]
-
-
-class CreateBook(BaseModel):
-    title: str
-    author: str
-    category: str
-
-
-class UpdateBook(BaseModel):
-    title: Optional[str] = None
-    author: Optional[str] = None
-    category: Optional[str] = None
 
 
 class Book(BaseModel):
@@ -36,15 +10,69 @@ class Book(BaseModel):
     title: str
     author: str
     category: str
+    description: str
+    rating: int
+
+
+class CreateBook(BaseModel):
+    title: str
+    author: str
+    category: str
+    description: str
+    rating: int
+
+
+class UpdateBook(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    category: Optional[str] = None
+    description: Optional[str] = None
+    rating: Optional[int] = None
+
+
+BOOKS = [
+    Book(
+        id=1,
+        title="El señor de los anillos",
+        author="JRR Tolkien",
+        category="novel",
+        description="EL mejor libro de todos los tiempos",
+        rating=5,
+    ),
+    Book(
+        id=2,
+        title="El hobbit",
+        author="JRR Tolkien",
+        category="novel",
+        description="El segundo mejor libro de todos los tiempos",
+        rating=5,
+    ),
+    Book(
+        id=3,
+        title="Harry Potter",
+        author="JK Rowling",
+        category="novel",
+        description="Harry potter es gay",
+        rating=1,
+    ),
+    Book(
+        id=4,
+        title="Harry Potter 2",
+        author="JK Rowling",
+        category="novel",
+        description="Hermioni esta buenisíma!",
+        rating=4,
+    ),
+]
 
 
 @app.post("/books")
 async def create_book(book: CreateBook) -> Book:
-    new_book = book.model_dump()
-    new_book["id"] = len(BOOKS) + 1
-
+    print(book)
+    new_book = Book(**book.model_dump(), id=len(BOOKS) + 1)
     BOOKS.append(new_book)
-    return Book(**new_book)
+
+    return new_book
 
 
 @app.get("/books")
@@ -71,7 +99,7 @@ async def read_all_books(
 @app.get("/books/{id}")
 async def read_book_by_id(id: int) -> Book:
     for b in BOOKS:
-        if b.get("id") == id:
+        if b.id == id:
             return b
     else:
         raise HTTPException(status_code=404, detail=f"{id} Not Found")
@@ -81,13 +109,17 @@ async def read_book_by_id(id: int) -> Book:
 async def update_book_by_id(id: int, book: UpdateBook) -> Book:
 
     for b in BOOKS:
-        if id == b.get("id"):
+        if id == b.id:
             if book.title is not None:
-                b["title"] = book.title
+                b.title = book.title
             if book.author is not None:
-                b["author"] = book.author
+                b.author = book.author
             if book.category is not None:
-                b["category"] = book.category
+                b.category = book.category
+            if book.description is not None:
+                b.description = book.description
+            if book.rating is not None:
+                b.rating = book.rating
 
             return b
 
@@ -95,8 +127,8 @@ async def update_book_by_id(id: int, book: UpdateBook) -> Book:
 
 
 @app.delete("/books/{id}")
-async def delete_book_by_id(id: int) -> None:
+async def delete_book_by_id(id: int):
     for book in BOOKS:
-        if book["id"] == id:
+        if book.id == id:
             BOOKS.remove(book)
             return None
